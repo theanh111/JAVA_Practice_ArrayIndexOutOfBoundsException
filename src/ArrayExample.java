2183770022e54279b298ae2d4dfd19453c927c27
anh.tran(c0720i1) <69283798+theanh111@users.noreply.github.com>
2020-09-29 11:16:30 +0700

    thuc hanh Sử dụng lớp ArrayIndexOutOfBoundsException

---
+++ src/ArrayExample.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class ArrayExample {
+    public Integer[] createRandom() {
+        Random random = new Random();
+        Integer[] array = new Integer[100];
+        System.out.println("Danh sách phần tử của mảng là: ");
+        for (int i = 0; i < 100; i++) {
+            array[i] = random.nextInt(100);
+            System.out.print(array[i] + " ");
+        }
+        return array;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayExample arrayExample = new ArrayExample();
+        Integer[] array = arrayExample.createRandom();
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("\nVui lòng nhập chỉ số của một phần tử bất kỳ: ");
+        int x = input.nextInt();
+        try {
+            System.out.println("Giá trị của phần tử có chỉ số " + x + " là " + array[x]);
+        }
+        catch (IndexOutOfBoundsException e ) {
+            System.out.println("Chỉ số vượt quá giới hạn của mảng!");
+        }
+        finally {
+            System.out.println("Hoàn tất chương trình.");
+        }
+    }
+}
